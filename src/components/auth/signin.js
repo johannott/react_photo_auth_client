e7a49dbe4aede1f61ebe6b36ecbb3477c12c6440
Johann Ott <johannott@Johanns-iMac.local>
2016-04-22 16:06:43 +0100

    Signin auth state etc

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
 
 class Signin extends Component {
   handleFormSubmit({ email, password }) {
     console.log(email, password);
 
     //Need to do something to log user in
+    this.props.signinUser({ email, password });
+  }
+
+  renderAlert(){
+    if (this.props.errorMessage){
+      return(
+        <div className="alert alert-danger">
+          <strong>Opps!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
   }
 
   render() {
@@ -19,15 +31,20 @@ class Signin extends Component {
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input {...password} className="form-control" />
+          <input {...password} type="password" className="form-control" />
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign in</button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state){
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password'],
-})(Signin);
+}, mapStateToProps, actions)(Signin);
