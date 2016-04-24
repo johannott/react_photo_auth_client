cc66f346ec0f3fcdcdeca36fd49bcacfe4ef47a9
Johann Ott <johann.ott@gmail.com>
2016-04-24 21:19:18 +0100

    Sign Up Component

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,6 +25,27 @@ export function signinUser({ email, password }){
   }
 }
 
+export function signupUser({ email, password }){
+  return function(dispatch){
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+    .then(response => {
+      // If request is good...
+      // - Update state to indicate user is authenticated
+      dispatch({ type: AUTH_USER});
+      // - Save the JWT token
+      localStorage.setItem('token', response.data.token);
+      // - redirect to the route - /feature
+       browserHistory.push('/feature');
+    })
+    .catch(() => {
+      // If request is bad...
+      // - Show an error to the user
+      dispatch(authError(response.data.error));
+    });
+  }
+}
+
 export function authError(error){
   return{
       type: AUTH_ERROR,
