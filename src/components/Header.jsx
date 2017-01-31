0c50de0032052100a202af032a8307509ce688f3
Johann Ott <johann.ott@gmail.com>
2017-01-31 20:44:49 +0000

    Add react-bootstrap and implement navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,37 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class Header extends Component {
-  renderLinks() {
+  renderLoginLinks() {
     if (this.props.authenticated) {
       // show a link to sign out
-      return <li className="nav-item">
-        <Link className="nav-link" to="/signout">Sign Out</Link>
-        </li>;
+      return <NavItem eventKey={4} href="#"><Link className="nav-link" to="/signout">Sign Out</Link></NavItem>;
     } else {
       //show a link to sign in or sign up
       return [
-        <li className="nav-item" key={1}>
-          <Link className="nav-link" to="/signin">Sign In</Link>
-        </li>,
-        <li className="nav-item" key={2}>
-          <Link className="nav-link" to="/signup">Sign Up</Link>
-        </li>
+          <NavItem eventKey={4} href="#"><Link className="nav-link" to="/signin">Sign In</Link></NavItem>,
+          <NavItem eventKey={5} href="#"><Link className="nav-link" to="/signup">Sign Up</Link></NavItem>
       ];
     }
   }
 
   render() {
     return (
-      <nav className="navbar navbar-light">
-        <Link to="/" className="navbar-brand">Redux Auth</Link>
-        <ul className="nav navbar-nav">
-          {this.renderLinks()}
-        </ul>
-      </nav>
+      <Navbar defaultExpanded fixedTop fluid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Photos Website</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Wedding</NavItem>
+            <NavItem eventKey={2} href="#">Honeymoon</NavItem>
+            <NavItem eventKey={3} href="#">Baby</NavItem>
+            {this.renderLoginLinks()}
+          </Nav>
+      </Navbar>
     );
   }
 }
