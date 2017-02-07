06dd1e672384d702a15335573ead7caf157d050d
Johann Ott <johann.ott@gmail.com>
2017-02-07 21:18:26 +0000

    Got the navbar working and styling fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,41 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class Header extends Component {
   renderLoginLinks() {
     if (this.props.authenticated) {
       // show a link to sign out
-      return <NavItem eventKey={4} href="#"><Link className="nav-link" to="/signout">Sign Out</Link></NavItem>;
+      return <NavItem eventKey={4} href="/signout">Sign Out</NavItem>;
     } else {
       //show a link to sign in or sign up
       return [
-          <NavItem eventKey={4} href="#"><Link className="nav-link" to="/signin">Sign In</Link></NavItem>,
-          <NavItem eventKey={5} href="#"><Link className="nav-link" to="/signup">Sign Up</Link></NavItem>
+          <NavItem eventKey={4} href="/signin">Sign In</NavItem>,
+          <NavItem eventKey={5} href="/signup">Sign Up</NavItem>
       ];
     }
   }
 
   render() {
     return (
-      <Navbar defaultExpanded fixedTop fluid>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">Photos Website</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">Wedding</NavItem>
-            <NavItem eventKey={2} href="#">Honeymoon</NavItem>
-            <NavItem eventKey={3} href="#">Baby</NavItem>
-            {this.renderLoginLinks()}
-          </Nav>
-      </Navbar>
+      <header className="custom_nav">
+        <Navbar defaultExpanded fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Photos Website</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+             <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">Wedding</NavItem>
+                <NavItem eventKey={2} href="#">Honeymoon</NavItem>
+                <NavItem eventKey={3} href="#">Baby</NavItem>
+              </Nav>
+              <Nav pullRight>{this.renderLoginLinks()}</Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      </header>
     );
   }
 }
